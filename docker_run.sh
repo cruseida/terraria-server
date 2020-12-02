d9cbc5497449aa1388a4651320dbd3e54a901de8
cruseida <67791258+cruseida@users.noreply.github.com>
2020-12-03 01:57:07 +0800

    Update docker_run.sh
    just changing configs for less edits after cloning.

--- docker_run.sh
+++ docker_run.sh
@@ -5,7 +5,7 @@
 docker run \
   -p 7777:7777 \
   -p 7878:7878 \
-  -e WORLD_FILENAME= \
+  -e WORLD_FILENAME=cutiehq_baby_edition.wld \
   -e TERRACORD_ENABLED=true \
   -e TERRACORD_BOT_TOKEN="<bot token>" \
   -e TERRACORD_CHANNEL_ID="<channel id>" \
@@ -14,7 +14,7 @@ docker run \
   -v $(pwd)/terraria_data/plugins:/plugins \
   -v $(pwd)/terraria_data/logs:/tshock/logs \
   $@ \
-  --name terraria-server \
+  --name cutiehq-terraria-01 \
   -it \
   --rm \
   cruseida/terraria:latest
